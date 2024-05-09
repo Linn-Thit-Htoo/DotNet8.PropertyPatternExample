7a6c82924dc01aee2d74d1dabc33bb3d52e1c32a
Linn Thit <lth1212001@gmail.com>
2024-05-09 15:52:32 +0630

    Add project files.

---
+++ DotNet8.PropertyPatternExample/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNet8.PropertyPatternExample;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Product product = new(1, "Product", 10000);
+
+        if (product is { Price: > 1000, ProductName: "Product" })
+        {
+
+        }
+    }
+}
+
+//public class Product
+//{
+//    public Product(long productId, string productName, int price)
+//    {
+//        ProductId = productId;
+//        ProductName = productName;
+//        Price = price;
+//    }
+
+//    public long ProductId { get; set; }
+//    public string ProductName { get; set; } = null!;
+//    public int Price { get; set; }
+//}
+
+public record Product(long ProductId, string ProductName, int Price);
